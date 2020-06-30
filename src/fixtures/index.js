4cceb659a8cf562f104ba405fdba6b2d0d3befcc
ChenxiiCheng <chenxic1011@gmail.com>
2020-06-29 22:21:20 -0400

    initial commit

---
+++ src/fixtures/index.js
@@ -0,0 +1,17 @@
+export const singularStory = {
+  by: 'Chenxii',
+  id: 1,
+  time: 1567209822,
+  title: 'Google Response',
+  url: 'https://something.com/hackernews',
+};
+
+export const storyIds = [1];
+
+export const emptySingularStory = {
+  by: undefined,
+  id: undefined,
+  time: undefined,
+  title: undefined,
+  url: undefined,
+};
